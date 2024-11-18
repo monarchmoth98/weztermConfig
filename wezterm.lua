e9a4ed885b1220d6583ca05ed13f32cad3a8a452
Edouard Finet <edouardf@apadmi.com>
2024-11-18 10:36:36 +0000

    Create Wezterm Config file. WIP

---
+++ wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require("wezterm")
+local config = {}
+
+-- Colour scheme
+config.color_scheme = "BlueDolphin"
+
+-- Tabs
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_max_width = 24
+
+return config
